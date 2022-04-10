83c300d191fc77b2e2c53ebd1a4004f010730c07
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-04-10 17:35:28 +0100

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- *main - 
+ *main - prints the phrase "Programming is like building a multilingual puzzle"
  *
- *prints the phrase "Programming is like building a multilingual puzzle"
  *Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
