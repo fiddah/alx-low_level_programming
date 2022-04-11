e6f5d91545d7651969fb5ea78e51fff8d58c1399
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-04-11 15:43:48 +0100

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,12 +5,11 @@
 /*betty style doc for function main goes there */
 /**
 * main - print if the number is postive, zero, or negative
-*
 * Description: using the main function
 * this program prints "Programming is positive, zero, or negative"
 * Return: 0
 */
-int main(void) 	
+int main(void)
 {
 int n;
 srand(time(0));
