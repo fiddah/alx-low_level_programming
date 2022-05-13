30c4863aa586b5221e39efc643bd594b9632980a
Fatima Tasallah Rufai <rufaifatimah@yahoo.com>
2022-05-13 17:20:37 +0100

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "function_pointers.h"
+
 /**
  * print_name - prints a name
  * @name: name to print
@@ -7,5 +8,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-(*f)(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
