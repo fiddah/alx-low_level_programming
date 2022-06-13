e39491d68df5cd7ef73323b8617674558ff91a5b
fiddah <49383326+fiddah@users.noreply.github.com>
2022-06-13 16:06:45 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,16 +6,14 @@
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
-
 size_t list_len(const list_t *h)
 {
-    size_t nelem;
-
-    nelem = 0;
-    while (h != NULL)
-    {
-        h = h->next;
-        nelem++;
-    }
-    return (nelem);
+size_t nelem;
+nelem = 0;
+while (h != NULL)
+{
+h = h->next;
+nelem++;
+}
+return (nelem);
 }
